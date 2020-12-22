4cdd46ada369edb5f145004f0805c12e3c1f5065
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-12-23 00:22:14 +0800

    done with products

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
-<h1>Prodotti</h1>
+    <div class="container">
+        <h1>Prodotti</h1>
+        <div class="card-cont">
+            @foreach ($pasta as  $singola_pasta)
+                <div class="card">
+                    <img src="{{$singola_pasta['src']}}" alt="{{$singola_pasta['titolo']}}">
+                    <div class="description">
+                        <a href="#">{{$singola_pasta['titolo']}}</a>
+
+                    </div>
+
+                </div>
+
+            @endforeach
+
+        </div>
+
+    </div>
 @endsection
 
 @section('page-title', 'Prodotti - La Molisana')
