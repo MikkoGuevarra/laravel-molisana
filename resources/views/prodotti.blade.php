8ad8bd9a70064cdbe00ec88c8b4d728f2279fe7a
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-01-06 14:36:45 +0800

    added bg-img

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        @foreach ($formati as $tipo => $pasta)
-            <h1>{{$tipo}}</h1>
-            <div class="card-cont">
-                @foreach ($pasta as $key => $formato)
-                    <div class="card">
-                        <img src="{{$formato['src']}}" alt="{{$formato['titolo']}}">
-                        <div class="description">
-                            <a href="{{route('details', ['id' => $key])}}">{{$formato['titolo']}}</a>
+    <div class="products">
+        <div class="container">
+            @foreach ($formati as $tipo => $pasta)
+                <h1>{{$tipo}}</h1>
+                <div class="card-cont">
+                    @foreach ($pasta as $key => $formato)
+                        <div class="card">
+                            <img src="{{$formato['src']}}" alt="{{$formato['titolo']}}">
+                            <div class="description">
+                                <a href="{{route('details', ['id' => $key])}}">{{$formato['titolo']}}</a>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
-
-            </div>
-        @endforeach
+                    @endforeach
+                </div>
+            @endforeach
+        </div>
 
     </div>
 @endsection
